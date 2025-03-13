267c3218174992bddab1ac90d0f66a0fb3a05767
tianlong.zhao <tianlong.zhao@huifu.com>
2025-03-13 15:36:43 +0800

    feat: 部分参数修改

--- src/const/settings/llm.ts
+++ src/const/settings/llm.ts
@@ -14,13 +14,13 @@ export const DEFAULT_LLM_CONFIG = genUserLLMConfig({
   },
 });
 
-export const DEFAULT_MODEL = 'gpt-4o-mini';
+export const DEFAULT_MODEL = 'deepseek-r1-distill-llama-70b';
 
-export const DEFAULT_EMBEDDING_MODEL = 'text-embedding-3-small';
-export const DEFAULT_EMBEDDING_PROVIDER = ModelProvider.OpenAI;
+export const DEFAULT_EMBEDDING_MODEL = 'text-embedding-v3';
+export const DEFAULT_EMBEDDING_PROVIDER = ModelProvider.Qwen;
 
 export const DEFAULT_RERANK_MODEL = 'rerank-english-v3.0';
 export const DEFAULT_RERANK_PROVIDER = 'cohere';
 export const DEFAULT_RERANK_QUERY_MODE = 'full_text';
 
-export const DEFAULT_PROVIDER = ModelProvider.OpenAI;
+export const DEFAULT_PROVIDER = ModelProvider.Qwen;
